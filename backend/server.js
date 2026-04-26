c5e796be6ccaa1b5b12b37435edc32ef3efeed19
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-04-26 14:55:35 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://social-app-hg1m.vercel.app"
+    "https://social-app-eight-topaz.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
